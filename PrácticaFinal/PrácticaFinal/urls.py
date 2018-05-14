69df4ccd51c25ef10aabfd19356793e87d97d048
margaft <marga_rita_ft@hotmail.com>
2018-05-14 09:23:33 +0200

    Cargados los museos en la base de datos

--- PrácticaFinal/PrácticaFinal/urls.py
+++ PrácticaFinal/PrácticaFinal/urls.py
@@ -14,7 +14,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from museos import views
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.pagina_principal, name = "Página principal de la práctica"),
+    
 ]
--- PrácticaFinal/museos/views.py
+++ PrácticaFinal/museos/views.py
@@ -1,3 +1,93 @@
 from django.shortcuts import render
+from museos.models import Museo, Comentario, Escogido, Estilo
+import xml.etree.ElementTree as ET
+from urllib.request import urlopen
+from django.http import HttpResponse
+from django.template.loader import get_template
+from django.template import Context
+from django.template import RequestContext
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
+@csrf_exempt
+def pagina_principal(peticion):
+   
+    if peticion.method == "POST":
+        if "boton" in peticion.POST:
+            eleccion = peticion.POST['boton']
+           
+           
+        else:
+            eleccion = ""
+            
+            # http://stackoverflow.com/questions/2792650/python3-error-import-error-no-module-name-urllib2
+            ArchivoXML = urlopen("https://datos.madrid.es/portal/site/egob/menuitem.ac61933d6ee3c31cae77ae7784f1a5a0/?vgnextoid=00149033f2201410VgnVCM100000171f5a0aRCRD&format=xml&file=0&filename=201132-0-museos&mgmtid=118f2fdbecc63410VgnVCM1000000b205a0aRCRD&preview=full")
+            tree = ET.parse(ArchivoXML)
+            root = tree.getroot()
+
+            for elemento in tree.iter():
+                if "ID-ENTIDAD" in elemento.attrib.values():   # Es un diccionario
+                    museo_nuevo = Museo(idmuseo = elemento.text)
+                elif "NOMBRE" in elemento.attrib.values():
+                    museo_nuevo.nombre = elemento.text
+                elif "DESCRIPCION-ENTIDAD" in elemento.attrib.values():
+                    museo_nuevo.descripcion_entidad = elemento.text
+                elif "HORARIO" in elemento.attrib.values():
+                    museo_nuevo.horario = elemento.text
+                elif "TRANSPORTE" in elemento.attrib.values():
+                    museo_nuevo.transporte = elemento.text
+                elif "ACCESIBILIDAD" in elemento.attrib.values():
+                    museo_nuevo.accesibilidad = elemento.text
+                elif "CONTENT-URL" in elemento.attrib.values():
+                    museo_nuevo.content_URL = elemento.text
+                elif "NOMBRE-VIA" in elemento.attrib.values():
+                    museo_nuevo.nombre_via = elemento.text
+                elif "CLASE-VIAL" in elemento.attrib.values():
+                    museo_nuevo.clase_via = elemento.text
+                elif "TIPO-NUM" in elemento.attrib.values():
+                    museo_nuevo.tipo_num = elemento.text
+                elif "NUM" in elemento.attrib.values():
+                    museo_nuevo.numero = elemento.text
+                elif "LOCALIDAD" in elemento.attrib.values():
+                    museo_nuevo.localidad = elemento.text
+                elif "PROVINCIA" in elemento.attrib.values():
+                    museo_nuevo.provincia = elemento.text
+                elif "CODIGO-POSTAL" in elemento.attrib.values():
+                    museo_nuevo.codigo_postal = elemento.text
+                elif "BARRIO" in elemento.attrib.values():
+                    museo_nuevo.barrio = elemento.text
+                elif "DISTRITO" in elemento.attrib.values():
+                    museo_nuevo.distrito = elemento.text
+                elif "COORDENADA-X" in elemento.attrib.values():
+                    museo_nuevo.coordenada_x = elemento.text
+                elif "COORDENADA-Y" in elemento.attrib.values():
+                    museo_nuevo.coordenada_y = elemento.text
+                elif "LATITUD" in elemento.attrib.values():
+                    museo_nuevo.latitud = elemento.text
+                elif "LONGITUD" in elemento.attrib.values():
+                    museo_nuevo.longitud = elemento.text
+                elif "TELEFONO" in elemento.attrib.values():
+                    museo_nuevo.telefono = elemento.text
+                elif "FAX" in elemento.attrib.values():
+                    museo_nuevo.fax = elemento.text
+                elif "EMAIL" in elemento.attrib.values():
+                    museo_nuevo.email = elemento.text
+                elif "TIPO" in elemento.attrib.values():
+                    museo_nuevo.save()
+                else:
+                    pass
+  
+
+    lista_museos = Museo.objects.all()
+    if len(lista_museos) == 0: #Si no hay ningun museo los cargamos
+        cargar = True
+    else:
+        cargar = False
+
+    template = get_template('pagina_principal.html')
+    context = RequestContext(peticion, {
+                                        'cargar': cargar})
+
+    resp = template.render(context)
+    return HttpResponse(resp)
